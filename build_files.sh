1f28e3494cd03ee3f1b6b21159628413192a454d
sameer266 <sameerbaiju792@gmail.com>
2025-03-01 17:35:41 +0545

    5th  commit

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,14 @@
+#!/bin/bash
+
 echo "BUILD START"
 
 # Check Python version
 python3.9 --version
 
-# Upgrade pip and ensure it’s installed
+# Upgrade pip and ensure it's installed
 python3.9 -m pip install --upgrade pip
-python3.9 -m ensurepip 
-
-pip install --upgrade pip
 
 # Install dependencies from requirements.txt
- pip install --no-cache-dir -r requirements.txt
-
-
-
+pip install --no-cache-dir -r requirements.txt
 
 echo "BUILD END"
