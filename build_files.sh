416de7a1dcf88dadd37fda30e5aff744868ad81a
sameer266 <sameerbaiju792@gmail.com>
2025-03-01 18:23:56 +0545

    10th  commit

--- build_files.sh
+++ build_files.sh
@@ -14,4 +14,13 @@ python3.9 -m pip install --upgrade pip
 # Install dependencies from requirements.txt
 python3.9 -m pip install --no-cache-dir -r requirements.txt
 
+
+echo "MAKE MIGRATIONS..."
+# Make and apply migrations
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput
+
 echo "BUILD END"
